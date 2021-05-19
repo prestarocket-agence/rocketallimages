814617fa32f27ef1169a16eafbf0e8e45c54ba73
DelecroixQuentin <quentin.delecroix@prestarocket.com>
2021-05-19 16:35:19 +0200

    CS-fixer & check controller is set in context (avoid notice in ws)

--- rocketallimages.php
+++ rocketallimages.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * NOTICE OF LICENSE
+ * NOTICE OF LICENSE.
+ *
  * @author      @prestarocket
  */
-
 if (!defined('_PS_VERSION_')) {
     exit;
 }
@@ -37,7 +37,8 @@ class rocketallimages extends Module
 
     public function uninstall()
     {
-        return parent::uninstall && Configuration::deleteByName('ROCKET_ALLIMAGES_ORDERED');
+        return parent::uninstall()
+            && Configuration::deleteByName('ROCKET_ALLIMAGES_ORDERED');
     }
 
     public function getContent()
@@ -45,6 +46,7 @@ class rocketallimages extends Module
         if (Tools::isSubmit('submitUpdate')) {
             Configuration::updateValue('ROCKET_ALLIMAGES_ORDERED', (bool) Tools::getValue('ROCKET_ALLIMAGES_ORDERED'));
         }
+
         return $this->renderForm();
     }
 
@@ -91,7 +93,7 @@ class rocketallimages extends Module
         $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
         $helper->identifier = $this->identifier;
         $helper->submit_action = 'submitUpdate';
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false) . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false).'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
         $helper->token = Tools::getAdminTokenLite('AdminModules');
         $helper->tpl_vars = array(
             'fields_value' => $this->getConfigFieldsValues(),
@@ -105,28 +107,27 @@ class rocketallimages extends Module
     public function getConfigFieldsValues()
     {
         return array(
-            'ROCKET_ALLIMAGES_ORDERED' => Tools::getValue('ROCKET_ALLIMAGES_ORDERED', Configuration::get('ROCKET_ALLIMAGES_ORDERED'))
+            'ROCKET_ALLIMAGES_ORDERED' => Tools::getValue('ROCKET_ALLIMAGES_ORDERED', Configuration::get('ROCKET_ALLIMAGES_ORDERED')),
         );
     }
 
-
     public function hookActionGetProductPropertiesAfter($params)
     {
-        if ($this->context->controller->php_self === 'product') {
-            if ( true == Configuration::get('ROCKET_ALLIMAGES_ORDERED')) {
+        if (null !== $this->context->controller && 'product' === $this->context->controller->php_self) {
+            if (true == Configuration::get('ROCKET_ALLIMAGES_ORDERED')) {
                 $combinations = Db::getInstance()->executeS(
                     'SELECT `id_product_attribute`
-                    FROM `' . _DB_PREFIX_ . 'product_attribute`
-                    WHERE `id_product` = ' . (int) $params['product']['id_product']
+                    FROM `'._DB_PREFIX_.'product_attribute`
+                    WHERE `id_product` = '.(int) $params['product']['id_product']
                 );
-                if ($this->haveImageForAllCombinations($combinations, $params['product']['id_product']) == true) {
+                if (true == $this->haveImageForAllCombinations($combinations, $params['product']['id_product'])) {
                     $params['product']['allimages'] = array();
                     $product = $params['product'];
                     $allimages = $this->imageRetriever->getProductImages(
                         $product,
                         $this->context->language
                     );
-                    foreach ($combinations as $combination) {                    
+                    foreach ($combinations as $combination) {
                         if ($product['id_product_attribute'] != $combination['id_product_attribute']) {
                             $product_next_combination = $params['product'];
                             $product_next_combination['id_product_attribute'] = $combination['id_product_attribute'];
@@ -134,10 +135,10 @@ class rocketallimages extends Module
                                 $product_next_combination,
                                 $this->context->language
                             );
-                        $allimages = array_merge($allimages, $allimages2);
+                            $allimages = array_merge($allimages, $allimages2);
                         }
                     }
-                    $allimages = array_unique($allimages, SORT_REGULAR);
+                    $allimages = array_unique($allimages, \SORT_REGULAR);
                     $params['product']['allimages'] = $allimages;
                 } else {
                     $params['product']['allimages'] = array();
@@ -147,17 +148,17 @@ class rocketallimages extends Module
                         $product,
                         $this->context->language
                     );
-                    $params['product']['allimages'] = $allimages;                
+                    $params['product']['allimages'] = $allimages;
                 }
             } else {
-                    $params['product']['allimages'] = array();
-                    $product = $params['product'];
-                    $product['id_product_attribute'] = 0;
-                    $allimages = $this->imageRetriever->getProductImages(
+                $params['product']['allimages'] = array();
+                $product = $params['product'];
+                $product['id_product_attribute'] = 0;
+                $allimages = $this->imageRetriever->getProductImages(
                         $product,
                         $this->context->language
                     );
-                    $params['product']['allimages'] = $allimages;
+                $params['product']['allimages'] = $allimages;
             }
         }
     }
@@ -165,14 +166,15 @@ class rocketallimages extends Module
     public function haveImageForAllCombinations($combinations, $id_product)
     {
         foreach ($combinations as $combination) {
-            $sql = 'SELECT `id_product_attribute` 
-                FROM `' . _DB_PREFIX_ . 'product_attribute_image` pai 
-                LEFT JOIN `' . _DB_PREFIX_ . 'image` im ON pai.id_image = im.id_image
-                WHERE im.id_product = ' . (int) $id_product . ' AND id_product_attribute = ' . (int) $combination['id_product_attribute'];
+            $sql = 'SELECT `id_product_attribute`
+                FROM `'._DB_PREFIX_.'product_attribute_image` pai
+                LEFT JOIN `'._DB_PREFIX_.'image` im ON pai.id_image = im.id_image
+                WHERE im.id_product = '.(int) $id_product.' AND id_product_attribute = '.(int) $combination['id_product_attribute'];
             if (!Db::getInstance()->getValue($sql)) {
                 return false;
             }
         }
+
         return true;
     }
 }
