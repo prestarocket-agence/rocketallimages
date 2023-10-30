4b9f0a266f9c87ea76de0fd491916d5d2f8a3e42
DelecroixQuentin <quentin.delecroix@prestarocket.com>
2023-10-30 09:27:07 +0100

    Refactor: Use DbQuery and simplify logic + clean repo

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # rocketallimages
 
 Add property all images in product lazy array
-![pain-au-chocola](pain-au-chocolat.jpg?raw=true "pain-au-chocola")
-
-![wtf](wtf.jpg?raw=true "wtf")
--- rocketallimages.php
+++ rocketallimages.php
@@ -2,7 +2,14 @@
 /**
  * NOTICE OF LICENSE.
  *
- * @author      @prestarocket
+ * This file is licenced under the Software License Agreement.
+ * With the purchase or the installation of the software in your application
+ * you accept the licence agreement.
+ * You must not modify, adapt or create derivative works of this source code
+ *
+ * @author      Prestarocket <prestarocket@gmail.com>
+ * @copyright   SARL JUST WEB
+ * @license     Commercial
  */
 if (!defined('_PS_VERSION_')) {
     exit;
@@ -21,10 +28,12 @@ class rocketallimages extends Module
         $this->version = '1.0.0';
         $this->author = 'Prestarocket';
         $this->bootstrap = true;
+        $this->need_instance = 0;
 
         parent::__construct();
 
-        $this->displayName = $this->l('Add all products images on product page');
+        $this->displayName = $this->l('Manage products images');
+        $this->description = $this->l('Add all products images on product page');
 
         $this->imageRetriever = new ImageRetriever($this->context->link);
     }
@@ -52,37 +61,37 @@ class rocketallimages extends Module
 
     public function renderForm()
     {
-        $fields_form = array(
-            'form' => array(
-                'legend' => array(
-                    'title' => $this->trans('Settings', array(), 'Admin.Global'),
+        $fields_form = [
+            'form' => [
+                'legend' => [
+                    'title' => $this->trans('Settings', [], 'Admin.Global'),
                     'icon' => 'icon-cogs',
-                ),
-                'input' => array(
-                    array(
+                ],
+                'input' => [
+                    [
                         'type' => 'switch',
-                        'label' => $this->trans('Display combinations image(s) First?', array(), 'Modules.rocketallimages.Admin'),
-                        'desc' => $this->trans('If enabled, this use more resources.', array(), 'Modules.rocketallimages.Admin'),
+                        'label' => $this->trans('Display combinations image(s) First?', [], 'Modules.rocketallimages.Admin'),
+                        'desc' => $this->trans('If enabled, this use more resources.', [], 'Modules.rocketallimages.Admin'),
                         'name' => 'ROCKET_ALLIMAGES_ORDERED',
-                        'values' => array(
-                            array(
+                        'values' => [
+                            [
                                 'id' => 'active_on',
                                 'value' => 1,
-                                'label' => $this->trans('Yes', array(), 'Admin.Global'),
-                            ),
-                            array(
+                                'label' => $this->trans('Yes', [], 'Admin.Global'),
+                            ],
+                            [
                                 'id' => 'active_off',
                                 'value' => 0,
-                                'label' => $this->trans('No', array(), 'Admin.Global'),
-                            ),
-                        ),
-                    ),
-                ),
-                'submit' => array(
-                    'title' => $this->trans('Save', array(), 'Admin.Actions'),
-                ),
-            ),
-        );
+                                'label' => $this->trans('No', [], 'Admin.Global'),
+                            ],
+                        ],
+                    ],
+                ],
+                'submit' => [
+                    'title' => $this->trans('Save', [], 'Admin.Actions'),
+                ],
+            ],
+        ];
 
         $helper = new HelperForm();
         $helper->show_toolbar = false;
@@ -93,55 +102,58 @@ class rocketallimages extends Module
         $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
         $helper->identifier = $this->identifier;
         $helper->submit_action = 'submitUpdate';
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false).'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false) . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
         $helper->token = Tools::getAdminTokenLite('AdminModules');
-        $helper->tpl_vars = array(
+        $helper->tpl_vars = [
             'fields_value' => $this->getConfigFieldsValues(),
             'languages' => $this->context->controller->getLanguages(),
             'id_language' => $this->context->language->id,
-        );
+        ];
 
-        return $helper->generateForm(array($fields_form));
+        return $helper->generateForm([$fields_form]);
     }
 
     public function getConfigFieldsValues()
     {
-        return array(
+        return [
             'ROCKET_ALLIMAGES_ORDERED' => Tools::getValue('ROCKET_ALLIMAGES_ORDERED', Configuration::get('ROCKET_ALLIMAGES_ORDERED')),
-        );
+        ];
     }
 
     public function hookActionGetProductPropertiesAfter($params)
     {
         if (null !== $this->context->controller && 'product' === $this->context->controller->php_self) {
             if (true == Configuration::get('ROCKET_ALLIMAGES_ORDERED')) {
-                $combinations = Db::getInstance()->executeS(
-                    'SELECT `id_product_attribute`
-                    FROM `'._DB_PREFIX_.'product_attribute`
-                    WHERE `id_product` = '.(int) $params['product']['id_product']
-                );
-                if (true == $this->haveImageForAllCombinations($combinations, $params['product']['id_product'])) {
-                    $params['product']['allimages'] = array();
+                $combinations = $this->getProductCombinations((int) $params['product']['id_product']);
+                if (null === $combinations) {
+                    return;
+                }
+
+                if (true === $this->haveImageForAllCombinations($combinations, (int) $params['product']['id_product'])) {
+                    $params['product']['allimages'] = [];
                     $product = $params['product'];
                     $allimages = $this->imageRetriever->getProductImages(
                         $product,
                         $this->context->language
                     );
+
                     foreach ($combinations as $combination) {
-                        if ($product['id_product_attribute'] != $combination['id_product_attribute']) {
-                            $product_next_combination = $params['product'];
-                            $product_next_combination['id_product_attribute'] = $combination['id_product_attribute'];
+                        if ((int) $product['id_product_attribute'] !== (int) $combination['id_product_attribute']) {
+                            $productNextCombination = $params['product'];
+                            $productNextCombination['id_product_attribute'] = $combination['id_product_attribute'];
                             $allimages2 = $this->imageRetriever->getProductImages(
-                                $product_next_combination,
+                                $productNextCombination,
                                 $this->context->language
                             );
+
                             $allimages = array_merge($allimages, $allimages2);
                         }
                     }
-                    $allimages = array_unique($allimages, \SORT_REGULAR);
+
+                    $allimages = array_unique($allimages, SORT_REGULAR);
                     $params['product']['allimages'] = $allimages;
                 } else {
-                    $params['product']['allimages'] = array();
+                    $params['product']['allimages'] = [];
                     $product = $params['product'];
                     $product['id_product_attribute'] = 0;
                     $allimages = $this->imageRetriever->getProductImages(
@@ -151,30 +163,48 @@ class rocketallimages extends Module
                     $params['product']['allimages'] = $allimages;
                 }
             } else {
-                $params['product']['allimages'] = array();
+                $params['product']['allimages'] = [];
                 $product = $params['product'];
                 $product['id_product_attribute'] = 0;
                 $allimages = $this->imageRetriever->getProductImages(
-                        $product,
-                        $this->context->language
-                    );
+                    $product,
+                    $this->context->language
+                );
                 $params['product']['allimages'] = $allimages;
             }
         }
     }
 
-    public function haveImageForAllCombinations($combinations, $id_product)
+    private function haveImageForAllCombinations(array $combinations, int $productId): bool
     {
         foreach ($combinations as $combination) {
-            $sql = 'SELECT `id_product_attribute`
-                FROM `'._DB_PREFIX_.'product_attribute_image` pai
-                LEFT JOIN `'._DB_PREFIX_.'image` im ON pai.id_image = im.id_image
-                WHERE im.id_product = '.(int) $id_product.' AND id_product_attribute = '.(int) $combination['id_product_attribute'];
-            if (!Db::getInstance()->getValue($sql)) {
+            if (false === $this->haveImageForThisCombination((int) $combination['id_product_attribute'], $productId)) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    private function haveImageForThisCombination(int $productAttributeId, int $productId): bool
+    {
+        return (bool) Db::getInstance()->getValue((new DbQuery())
+            ->select('pai.`id_product_attribute`')
+            ->from('product_attribute_image', 'pai')
+            ->leftJoin('image', 'im', 'pai.`id_image` = im.`id_image`')
+            ->where('im.`id_product` = ' . (int) $productId)
+            ->where('pai.`id_product_attribute` = ' . (int) $productAttributeId)
+        );
+    }
+
+    private function getProductCombinations(int $productId): ?array
+    {
+        $combs = Db::getInstance()->executeS((new DbQuery())
+            ->select('`id_product_attribute`')
+            ->from('product_attribute')
+            ->where('`id_product` = ' . (int) $productId)
+        );
+
+        return $combs ? $combs : null;
     }
 }
